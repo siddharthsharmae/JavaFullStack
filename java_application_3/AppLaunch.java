7c3b1a21edef0ecfb79a10f141fdebe24b534c08
siddharth.sharma212004@gmail.com <Siddharth>
2024-11-08 08:02:46 +0530

    Moving car Program

---
+++ java_application_3/AppLaunch.java
@@ -0,0 +1,10 @@
+package Application.Swing.java_application_3;
+
+import java.io.IOException;
+
+public class AppLaunch {
+	public static void main(String[] args) throws IOException {
+		new AppFrame();
+
+	}
+}
